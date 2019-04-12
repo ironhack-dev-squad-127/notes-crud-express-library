b5f4edfab039d8001e9b072f57f8e33d0b9682dd
Maxence Bouret <maxence.bouret@gmail.com>
2019-04-12 15:03:50 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
     })
 });
 
-// http://localhost:3000/books/5caf71777412fd8c8759ce80
+// Example: http://localhost:3000/books/5caf71777412fd8c8759ce80
 router.get('/books/:bookId', (req,res,next) => {
   console.log("The id is", req.params.bookId)
   Book.findById(req.params.bookId)
@@ -21,6 +21,18 @@ router.get('/books/:bookId', (req,res,next) => {
       })
     })
 })
+
+// Route GET /create-book to display the form to add a book
+router.get('/create-book', (req,res,next)=> {
+  res.render('create-book') // render "views/create-book.hbs"
+})
+
+// Route POST /create-book to receive the form submission
+router.post('/create-book', (req,res,next)=> {
+  console.log("req.body", req.body)
+})
+
+
 
 router.get('/surprise', (req, res, next) => {
   next(42)
